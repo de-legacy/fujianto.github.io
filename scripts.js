4bf146153970a29ee9181dfca3598338ff1ea1b0
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-09-05 11:24:00 +0700

    Adding simple js validation

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var btnSendMail = document.getElementById('send-mail');
+var yourName = document.getElementById('yourName');
+var yourEmail = document.getElementById('yourEmail');
+var emailMessage = document.getElementById('emailMessage');
+
+btnSendMail.addEventListener('click', function() {
+
+	if (yourName.value === "" || yourEmail.value === "" || emailMessage.value === "") {
+		alert("Please fill all your form data correctly");
+
+		return true;
+	} else {
+		alert("Thank You! Your Email has been Sent");
+
+		return true;
+	}
+	
+})
